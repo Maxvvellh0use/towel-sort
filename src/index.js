47528fc5701a73f2783ab4a72a7eae493ab2965d
Maksim Pavlov <pavlovmark100@gmail.com>
2020-02-20 23:30:59 +0400

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if (!arguments.length || !matrix.length) {
+        return []
+    }
+    const mapMatrix = matrix.map( function(elem, i) {
+        if (i % 2 !== 0) {
+            return elem.sort((a,b) => b - a)
+        }
+        else {
+            return elem
+        }
+    });
+    console.log(mapMatrix);
+    const concatMatrix = mapMatrix.concat(...matrix);
+    const result = concatMatrix.filter(elem => Array.isArray(elem) === false);
+    console.log(result);
+    return result;
+};
+
+// console.log(towelSort());
